401cbca4898aeee54161c3de2ab6e29bf40130e0
Anton Pirker <anton@ignaz.at>
2016-04-30 23:44:35 +0200

    Changed value for philipines

--- country_data.js
+++ country_data.js
@@ -907,7 +907,7 @@ var country_data = [
     },
     {
         code: "PH",
-        value: 1,
+        value: 100,
         name: "Philippines",
         description: '<b>Hotel am Brillantengrund</b><br/>, Bandgasse 4, 1070 Wien<br/><a href="http://www.brillantengrund.com/" target="_blank">http://www.brillantengrund.com/</a>'
     },
